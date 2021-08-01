6c4ea35dd24decf91d1dba3b01412075bd1647e0
ianubos <1031gonshi@gmail.com>
2021-08-01 18:01:50 +0900

    spacer modified

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React from "react"
 import Layout from '../components/layout/Layout'
-import BackPicture from "../components/BackPicture";
-import TopCategory from "../components/TopCategory";
+import BackPicture from "../components/BackPicture"
+import TopCategory from "../components/TopCategory"
+import styled from 'styled-components'
 
-const Spacer = () => <span className="h-96 sm:h-screen block" style={{minHeight: 600}}></span>;
+const Spacer = styled.span`
+  display: block;
+  height: 100vh;
+  @media screen and (max-width: 1000px) {
+    max-height: 600px;
+  }
+`
 
 function Index() {
     return (
